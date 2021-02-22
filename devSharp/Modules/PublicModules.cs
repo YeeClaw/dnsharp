ffce367e4fc783b3e1c2a6149bc137bd6a450cbe
YeeClaw <austin@jcolt.net>
2021-02-22 14:47:41 -0700

    Clean up PublicModules.cs

--- devSharp/Modules/PublicModules.cs
+++ devSharp/Modules/PublicModules.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.IO;
-using System.Threading.Channels;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
-using Discord.API;
-using devSharp.Services;
-using Discord.WebSocket;
 
 namespace devSharp.Modules
 {
@@ -32,36 +27,5 @@ namespace devSharp.Modules
 
             await ReplyAsync(user.ToString());
         }
-
-        /*
-        [Command("ban")]
-        [RequireContext(ContextType.Guild)]
-        // make sure the user invoking the command can ban
-        [RequireUserPermission(GuildPermission.BanMembers)]
-        // make sure the bot itself can ban
-        [RequireBotPermission(GuildPermission.BanMembers)]
-        public async Task BanUserAsync(IGuildUser user, [Remainder] string reason = null)
-        {
-            await user.Guild.AddBanAsync(user, reason: reason);
-            await ReplyAsync("ok!");
-        }
-        */
-
-        // [Remainder] takes the rest of the command's arguments as one argument, rather than splitting every space
-        [Command("echo")]
-        public Task EchoAsync([Remainder] string text)
-            // Insert a ZWSP before the text to prevent triggering other bots!
-            => ReplyAsync('\u200B' + text);
-
-        // 'params' will parse space-separated elements into a list
-        [Command("list")]
-        public Task ListAsync(params string[] objects)
-            => ReplyAsync("You listed: " + string.Join("; ", objects));
-
-        // Setting a custom ErrorMessage property will help clarify the precondition error
-        [Command("guild_only")]
-        [RequireContext(ContextType.Guild, ErrorMessage = "Sorry, this command must be ran from within a server, not a DM!")]
-        public Task GuildOnlyCommand()
-            => ReplyAsync("Nothing to see here!");
     }
 }
