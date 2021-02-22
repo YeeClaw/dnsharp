df1c08388d5a337a5c306eeb4ee64447f3c9ea0a
YeeClaw <austin@jcolt.net>
2021-02-22 16:10:40 -0700

    Add first implimentation of proud points (fix nickname error)

--- devSharp/Modules/PublicModules.cs
+++ devSharp/Modules/PublicModules.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
+using Discord.WebSocket;
 
 namespace devSharp.Modules
 {
@@ -11,14 +12,22 @@ namespace devSharp.Modules
 
         [Command("ping")]
         [Alias("pong", "hello")]
-        public async Task PingAsync()
+        public async Task PingAsync(String pong = null)
         {
-            long time = DateTime.Now.Millisecond;
+            if (pong == "pong")
+            {
+                await Context.Channel.SendMessageAsync("That's not how this works");
+            }
+            else
+            {
+                long time = DateTime.Now.Millisecond;
             
-            var message = await Context.Channel.SendMessageAsync("> Pong! :ping_pong:");
-            await message.ModifyAsync(msg => msg.Content = "> Pong! :ping_pong:\n" + (DateTime.Now.Millisecond - time) + "ms");
+                var message = await Context.Channel.SendMessageAsync("> Pong! :ping_pong:");
+                await message.ModifyAsync(msg => msg.Content = "> Pong! :ping_pong:\n" + (DateTime.Now.Millisecond - time) + "ms");
+            }
+            
         }
-
+        
         // Get info on a user, or the user who invoked the command if one is not specified
         [Command("userinfo")]
         public async Task UserInfoAsync(IUser user = null)
@@ -26,6 +35,19 @@ namespace devSharp.Modules
             user = user ?? Context.User;
 
             await ReplyAsync(user.ToString());
+        }
+
+        [Command("+pp")]
+        public async Task ProudPointsAsync(IUser user, int amount)
+        {
+            if (Context.User.Id == 190525744907157505)
+            {
+                await Context.Channel.SendMessageAsync(user + " has gained " + amount + " proud point(s)!");
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync("You don't have permission to use this command");
+            }
         }
     }
 }
